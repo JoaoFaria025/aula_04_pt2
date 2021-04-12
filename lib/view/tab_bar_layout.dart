381195fd36cdd1a0a5bfd86e683b10d06c35e78c
João Victor Corsi Ferreira <32347104+jvccorsi@users.noreply.github.com>
2021-04-12 20:46:40 -0300

    commit finnale

--- lib/view/tab_bar_layout.dart
+++ lib/view/tab_bar_layout.dart
@@ -3,39 +3,46 @@ import 'package:aula_04_pt2/view/tela2/tela2.dart';
 import 'package:aula_04_pt2/view/tela3/tela3.dart';
 import 'package:flutter/material.dart';
 
-
 class MyTabBarLAYOUT extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(primarySwatch: Colors.pink),
         home: DefaultTabController(
-          length: 3,
-          child: Scaffold(
-            appBar: AppBar(
-                title: Text("Apresentation"),
-                bottom: TabBar(
-                  tabs: [
-                    Tab(
-                      icon: Icon(Icons.assignment_ind),
-                      //child: Text("João Pedro de Faria"),
-                    ),
-                    Tab(
-                      icon: Icon(Icons.assignment_ind),
-                    ),
-                    Tab(
-                      icon: Icon(Icons.app_registration),
-                    ),
-                  ],
-                )),
-            body: TabBarView(
-              children: [
-                  tela1(),
-                  tela2(),
-                  tela3(),
-              ],
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+            backgroundColor: Color.fromRGBO(12, 69, 122, 1),
+            title: Center(
+              child: Text(
+                "Minios Store",
+                style: TextStyle(
+                    letterSpacing: 2,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
             ),
-          ),
-        ));
+            bottom: TabBar(
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.assignment_ind),
+                  //child: Text("João Pedro de Faria"),
+                ),
+                Tab(
+                  icon: Icon(Icons.assignment_ind),
+                ),
+                Tab(
+                  icon: Icon(Icons.app_registration),
+                ),
+              ],
+            )),
+        body: TabBarView(
+          children: [
+            tela1(),
+            tela2(),
+            tela3(),
+          ],
+        ),
+      ),
+    ));
   }
 }
--- lib/view/tela2/tela2.dart
+++ lib/view/tela2/tela2.dart
@@ -4,48 +4,53 @@ import 'package:flutter/material.dart';
 class tela2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return (
-      Center(
-      child:Container(
-        child:Column(
-          children: [
-            photo_my(),
-            name_my(),
-            info_my(),
-          ]
-          )
-      )
-    ));
+    return (Center(
+        child: Container(
+            child: Column(children: [
+      photo_my(),
+      Divider(),
+      name_my(),
+      info_my(),
+    ]))));
   }
 }
 
- Widget photo_my() {
-   return Container(
-     decoration: BoxDecoration(
-       borderRadius: BorderRadius.circular(25),
-       color: Colors.cyan,
-       boxShadow: [BoxShadow(color:Colors.grey,blurRadius: 20)],
-      ),
-     width: 250,
-     height:250,
-     margin: EdgeInsets.all(15),
-   );
- }
+Widget photo_my() {
+  return Container(
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(25),
+      boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 5)],
+    ),
+    child: ClipRRect(
+      borderRadius: BorderRadius.circular(30),
+      child: Image.asset('assets/pp.jpg'),
+    ),
+    width: 250,
+    height: 250,
+    margin: EdgeInsets.all(20),
+  );
+}
 
- Widget name_my() {
-   return Container(
-     child:Text("João Victor Corsi Ferreira",style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-   );
- }
+Widget name_my() {
+  return Container(
+    child: Text("João Victor Corsi Ferreira",
+        style: TextStyle(
+          fontSize: 25,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2,
+        )),
+    margin: EdgeInsets.only(top: 10),
+  );
+}
 
- Widget info_my() {
-   return Center(
-     child:Container(
-       child:Text("Sou 019 estou no 5ºsemestre de BSI. Gosto bastante de praticar atividade física e atualmente participio da LIESTAG.",
-       style: TextStyle(fontSize: 20,letterSpacing:2),
-       textAlign: TextAlign.center,
-        ),
-        margin: EdgeInsets.all(15),
-     )
-   );
- }
+Widget info_my() {
+  return Center(
+      child: Container(
+    child: Text(
+      "Sou 019 estou no 5ºsemestre de BSI. Gosto bastante de praticar atividade física, principalmente academia.",
+      style: TextStyle(fontSize: 20, letterSpacing: 2),
+      textAlign: TextAlign.center,
+    ),
+    margin: EdgeInsets.all(15),
+  ));
+}
